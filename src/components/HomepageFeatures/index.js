aa76d3d164fe1ab648f346704f5f566d75e7a97c
Gabriele Groppo <gabriele.groppo03@gmail.com>
2025-10-24 22:41:28 +0200

    KubePattern documentation start

---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,61 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Easy to use',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Deploy KubePattern on your cluster with read permissions, then trigger analysis via REST endpoint or automate it with CronJobs—simple and flexible.
+      </>
+    ),
+  },
+  {
+    title: 'Pattern As Code',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Define architectural patterns in declarative JSON files, read and enforced by <b>KubePattern</b> across your Kubernetes workloads.
+      </>
+    ),
+  },
+  {
+    title: 'Improve cluster quality',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        detect anti-patterns and missing best practices in your workloads. KubePattern ensures architectural consistency and helps maintain healthy, production-ready Kubernetes clusters.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
